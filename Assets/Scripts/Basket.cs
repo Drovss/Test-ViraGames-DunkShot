4ef961a112077f38ff68feffa1ad721648a3f7b7
Maksym <drovss@ukr.net>
2022-09-03 16:31:20 +0300

    clear comments

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,46 +1,25 @@
 using UnityEngine;
 
-public class Slingshot : MonoBehaviour
+public class Basket : MonoBehaviour
 {
-    //[SerializeField] private LineRenderer[] _lineRenderers;
-    //[SerializeField] private Transform[] _stripPositions;
     [SerializeField] private Transform _center;
     [SerializeField] private Transform _idlePosition;
     [SerializeField] private float _maxLength;
     [SerializeField] private Trajectory _trajectory;
     [SerializeField] private Transform _basket;
-    
-    
     [SerializeField] private GameObject _ballPrefab;
     [SerializeField] private float _ballPositionOffset;
     [SerializeField] private float _force;
 
     private Ball _ball;
-    //private Rigidbody2D _ballRb;
-    //private Collider2D _ballCol;
-    
     private bool _isMouseDown;
     private Vector3 _currentPosition;
-    
     private Camera _camera;
-    
-    // private Vector2 _startPoint;
-    // private Vector2 _endPoint;
-    // private Vector2 _direction;
-    // private Vector2 _force2;
-    // private float _distance;
-    //
+
     private void Start()
     {
         _camera = Camera.main;
-        
-        // for (int i = 0; i < _lineRenderers.Length; i++)
-        // {
-        //     _lineRenderers[i].positionCount = 2;
-        //     _lineRenderers[i].SetPosition(0, _stripPositions[i].position);
-        // }
-        //
-        //ball
+
         CreateBall();
     }
 
@@ -54,16 +33,6 @@ public class Slingshot : MonoBehaviour
             _trajectory.UpdateDots(_ball.transform.position, ballForce);
 
             RotateBasket();
-            
-            //ball
-            // if (_ballCol)
-            // {
-            //     _ballCol.enabled = true;
-            // }
-        }
-        else
-        {
-            //ResetStrips();
         }
     }
 
@@ -87,20 +56,7 @@ public class Slingshot : MonoBehaviour
         var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         _basket.rotation = Quaternion.Euler(0, 0, angle + 90);
     }
-    
-    // private void OnDrag()
-    // {
-    //     _endPoint = _camera.ScreenToWorldPoint(Input.mousePosition);
-    //     _distance = Vector2.Distance(_startPoint, _endPoint);
-    //     _direction = (_startPoint - _endPoint).normalized;
-    //     //_force = _direction * (_distance * _pushForce);
-    //     
-    //     //_trajectory.updateDots(_ball.Pos, _force2);
-    //     
-    //     Debug.DrawLine(_startPoint, _endPoint);
-    // }
-    
-    
+
 
     private void OnMouseDown()
     {
@@ -108,9 +64,7 @@ public class Slingshot : MonoBehaviour
         {
             return;
         }
-        
-        // _startPoint = _camera.ScreenToWorldPoint(Input.mousePosition);
-        
+
         _isMouseDown = true;
         _trajectory.Show();
     }
@@ -124,7 +78,6 @@ public class Slingshot : MonoBehaviour
         
         _isMouseDown = false;
         _trajectory.Hide();
-        //ball
         Shoot();
     }
 
@@ -136,51 +89,26 @@ public class Slingshot : MonoBehaviour
     
     private void SetStrips(Vector3 position)
     {
-        // for (int i = 0; i < _lineRenderers.Length; i++)
-        // {
-        //     _lineRenderers[i].SetPosition(1, position);
-        // }
-    
-        //ball
         if (_ball)
         {
             _ball.SetPosition(position, _center.position, _ballPositionOffset);
-            // var dir = position - _center.position;
-            // var transform1 = _ballRb.transform;
-            // transform1.position = position + dir.normalized * _ballPositionOffset;
-            // transform1.right = -dir.normalized;
         }
     }
 
     private void CreateBall()
     {
         var ball = Instantiate(_ballPrefab);
-
         _ball = ball.GetComponent<Ball>();
-        //_ballRb = ball.GetComponent<Rigidbody2D>();
-        //_ballCol = ball.GetComponent<Collider2D>();
-
-        //_ballCol.enabled = false;
-        //_ballRb.isKinematic = true;
         _ball.DeactivateRb();
-        
-        
-
         ResetStrips();
     }
 
     private void Shoot()
     {
-        //_ballRb.isKinematic = false;
         _ball.ActivateRb();
         Vector3 ballForce = (_currentPosition - _center.position) * (_force * -1);
         _ball.Push(ballForce);
-        //_ballRb.velocity = ballForce;
-
         _ball = null;
-        // _ballRb = null;
-        // _ballCol = null;
         Invoke(nameof(CreateBall), 2);
     }
-    
 }
